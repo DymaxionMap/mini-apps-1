65d9c14f36f740012f393812aa3e9a18a5f88f35
DymaxionMap <sthliao@gmail.com>
2018-12-10 12:01:44 -0800

    Create skeleton for app

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
+// MODEL
 
+const initializeGame = () => {};
+const updateCurrentPlayer = () => {};
+const updateBoard = () => {};
+const checkForWinner = () => {};
+const resetGame = () => {};
 
+// VIEW
+const renderBoard = () => {};
+const renderWinnerDisplay = () => {};
+const renderResetButton = () => {};
+
+// CONTROLLER
+const boardClickHandler = () => {};
+const resetClickHandler = () => {};
