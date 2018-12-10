6afa56c2e05b515c02618f2feb997ed5137dfc65
DymaxionMap <sthliao@gmail.com>
2018-12-10 12:22:54 -0800

    Implement game initialization and current player update

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,22 @@
 // MODEL
 
-const initializeGame = () => {};
-const updateCurrentPlayer = () => {};
+// Constants
+const X = 'X';
+const O = 'O';
+const TIE = 'TIE';
+const EMPTY = '';
+
+const initializeGame = () => ({
+  currentPlayer: X,
+  gameEnded: false,
+  winner: EMPTY,
+  board: [[EMPTY, EMPTY, EMPTY, ], [EMPTY, EMPTY, EMPTY, ], [EMPTY, EMPTY, EMPTY, ], ],
+});
+
+const updateCurrentPlayer = (game, player) => (
+  Object.assign({}, game, { currentPlayer:  player })
+);
+
 const updateBoard = () => {};
 const checkForWinner = () => {};
 const resetGame = () => {};
@@ -14,3 +29,12 @@ const renderResetButton = () => {};
 // CONTROLLER
 const boardClickHandler = () => {};
 const resetClickHandler = () => {};
+
+// TESTING
+const test = () => {
+  const game = initializeGame();
+  console.log(game);
+  console.log(updateCurrentPlayer(game, O));
+};
+
+test();
