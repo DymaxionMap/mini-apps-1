db7fa47e941ae9ac0dc4dc6be71b45f1336bddd8
DymaxionMap <sthliao@gmail.com>
2018-12-10 13:45:53 -0800

    Implement updating the game board model

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,6 +5,7 @@ const X = 'X';
 const O = 'O';
 const TIE = 'TIE';
 const EMPTY = '';
+const BOARD_SIZE = 3;
 
 const initializeGame = () => ({
   currentPlayer: X,
@@ -13,11 +14,30 @@ const initializeGame = () => ({
   board: [[EMPTY, EMPTY, EMPTY, ], [EMPTY, EMPTY, EMPTY, ], [EMPTY, EMPTY, EMPTY, ], ],
 });
 
-const updateCurrentPlayer = (game, player) => (
-  Object.assign({}, game, { currentPlayer:  player })
-);
+const updateCurrentPlayer = (game, player) => {
+  return Object.assign({}, game, { currentPlayer:  player });
+};
 
-const updateBoard = () => {};
+const generateUpdatedBoard = (board, currentPlayer, placedRow, placedCol) => {
+  const newBoard = [];
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    newBoard.push([]);
+    for (let col = 0; col < BOARD_SIZE; col++) {
+      if (row === placedRow && col === placedCol) {
+        newBoard[row].push(currentPlayer);
+      } else {
+        newBoard[row].push(board[row][col]);
+      }
+    }
+  }
+  
+  return newBoard;
+};
+
+const updateBoard = (game, placedRow, placedCol) => {
+  return Object.assign({}, game, { board:  generateUpdatedBoard(game.board, game.currentPlayer, placedRow, placedCol) });
+};
+
 const checkForWinner = () => {};
 const resetGame = () => {};
 
@@ -32,9 +52,28 @@ const resetClickHandler = () => {};
 
 // TESTING
 const test = () => {
-  const game = initializeGame();
-  console.log(game);
-  console.log(updateCurrentPlayer(game, O));
+  // ======== Test initializeGame ==========
+  // let game = initializeGame();
+  // console.log('intial game:', game);
+  
+  // ======== Test updateCurrentPlayer ==========
+  // let game = initializeGame();
+  // game = updateCurrentPlayer(game, O);
+  // console.log('updated player:', game);
+  
+  // ======== Test generateUpdatedBoard ==========
+  // let game = initializeGame();
+  // let updatedBoard = generateUpdatedBoard(game.board, game.currentPlayer, 1, 1);
+  // console.log('updated board 1:', updatedBoard);
+  // updatedBoard = generateUpdatedBoard(updatedBoard, X, 1, 3);
+  // console.log('updated board 2:', updatedBoard);
+  // updatedBoard = generateUpdatedBoard(updatedBoard, O, 0, 0);
+  // console.log('updated board 3:', updatedBoard);
+  
+  // // ======== Test updateBoard ==========
+  let game = initializeGame();
+  game = updateBoard(game, 1, 1);
+  console.log('placed X on (1, 1):\n', game);
 };
 
 test();
