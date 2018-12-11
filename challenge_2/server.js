b2b39ac468415a068a776768834846055da59320
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-11 14:47:08 -0800

    Retrieve body from request

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
   res.sendfile(path.join(__dirname, 'client', 'index.html'));
 });
 
-app.post('/', (req, res) => {
-  console.log('Someone posted something to /');
+app.get('/report', (req, res) => {
+  res.redirect('/');
+});
+
+app.post('/report',(req, res) => {
+  console.log(req.body.reportText);
+  res.redirect('/');
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
