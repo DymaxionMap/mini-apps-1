fd171310cac44e0e198bf745eb664f7ed74d94af
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-11 11:35:55 -0800

    Serve static index.html page

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('client'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
