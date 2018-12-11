b7fdaa8b4791a9e5d6d345f1887bad87637595e8
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-11 12:05:25 -0800

    Add get route to root url

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, 'client', 'index.html'));
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
