dc00fca98d7efcd5e81754b81d176281563ea08e
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-12 12:41:02 -0800

    Get React running

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,11 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+// const { Component } = React;
+// const { render } = ReactDOM;
+
+const App = props => (
+  <h1>Hello World</h1>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
