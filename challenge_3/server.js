2aa2d08f0e494887314e30252a03ce72353ba47b
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-12 11:49:59 -0800

    Initialize server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.get('/', (req, res) => {
+  console.log('Got a get request!');
+  res.sendStatus(200);
+});
 
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
