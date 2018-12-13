05cb0961715dade969d3c2f12ee2930f994cba94
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-12-13 12:33:11 -0800

    Configure dependencies for connect four

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['css-loader', 'style-loader'],
+      },
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        }
+      }
+    ],
+  },
+}
